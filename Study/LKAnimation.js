87a92970c0f30047803fa44fba5fe051fd4e2828
shiwei <348458804@qq.com>
2019-01-03 17:11:34 +0800

    动画第二种方式

--- Study/LKAnimation.js
+++ Study/LKAnimation.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Animated, StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+import { Animated, StyleSheet, Text, View, ScrollView, Image,LayoutAnimation,TouchableOpacity} from 'react-native';
 // Animated 可用的动画组件是Text，View，ScrollView和Image，
 class FadeInView extends React.Component {
     state = {
@@ -51,7 +51,6 @@ class FadeInView extends React.Component {
     }
   
     render() {
-      let { fadeAnim } = this.state;
   
       return (
         <Animated.View                 // 使用专门的可动画化的View组件
@@ -68,7 +67,132 @@ class FadeInView extends React.Component {
       );
     }
   }
-// 组合动画
+// LayoutAnimation
+class LKLayoutView extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            w:200,
+            h:20,
+            showNewOne:false 
+        }
+    }
+    pressAction() {
+        let thisPage = this
+        //下面代表的是当有布局改变的时候会以动画的形式过渡，
+        LayoutAnimation.configureNext({
+            duration: 1200,  //持续时间
+            create: ({//新布局的动画类型
+                type: 'linear', //线性模式  spring 弹跳  linear 线性  easeInEaseOut缓入淡出  easeIn缓入   easeOut缓出  keyboard键入
+                property:'opacity' //动画属性
+            }),
+            update: ({ //布局更新的动画类型
+                type:'spring',
+                springDamping: 0.4  //弹跳阻尼系数
+
+            })
+        })
+        if (this.state.h<100){
+            thisPage.setState({
+                w:this.state.w + 30,
+                h:this.state.h + 30,
+                showNewOne : true,
+            })
+        }
+       
+    }
+    render(){
+
+        return (
+            <View style={{...this.props.style}}>
+                <View style={{backgroundColor:'yellow',width:this.state.w,height:this.state.h}}>
+                    <Text style={{textAlign:'center',lineHeight:this.state.h}}>这个是现成的</Text>
+                </View>
+                {this.state.showNewOne ?(
+                <View style={{backgroundColor:'purple',width:this.state.w,height:this.state.h}}>
+                    <Text style={{textAlign:'center',lineHeight:this.state.h}}>这个是新添加的</Text>
+                </View>
+            ):null}
+            {/* 下面的onPress绑定的方法，如果需要修改state的值，必须要bind(this),否则state获取不到 */}
+                <TouchableOpacity onPress={this.pressAction.bind(this)}>
+                    <View style={{width:100,height:50,backgroundColor:'black'}}>
+                        <Text style={{color:'white',textAlign:'center',lineHeight:50}}>点击开始</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            
+           
+            
+
+        )
+
+    }
+
+}
+
+// 使用props,直接动态改变style(优化版)使用的是setNativeProps
+class LKChangePropsView extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            w:200,
+            h:20,
+            showNewOne:false 
+        }
+    }
+    pressAction() {
+        let thisPage = this
+        //下面代表的是当有布局改变的时候会以动画的形式过渡，
+        LayoutAnimation.configureNext({
+            duration: 1200,  //持续时间
+            create: ({//新布局的动画类型
+                type: 'linear', //线性模式  spring 弹跳  linear 线性  easeInEaseOut缓入淡出  easeIn缓入   easeOut缓出  keyboard键入
+                property:'opacity' //动画属性
+            }),
+            update: ({ //布局更新的动画类型
+                type:'spring',
+                springDamping: 0.4  //弹跳阻尼系数
+
+            })
+        })
+        if (this.state.h<100){
+            thisPage.setState({
+                w:this.state.w + 30,
+                h:this.state.h + 30,
+                showNewOne : true,
+            })
+        }
+       
+    }
+    render(){
+
+        return (
+            <View style={{...this.props.style}}>
+                <View style={{backgroundColor:'yellow',width:this.state.w,height:this.state.h}}>
+                    <Text style={{textAlign:'center',lineHeight:this.state.h}}>这个是现成的</Text>
+                </View>
+                {this.state.showNewOne ?(
+                <View style={{backgroundColor:'purple',width:this.state.w,height:this.state.h}}>
+                    <Text style={{textAlign:'center',lineHeight:this.state.h}}>这个是新添加的</Text>
+                </View>
+            ):null}
+            {/* 下面的onPress绑定的方法，如果需要修改state的值，必须要bind(this),否则state获取不到 */}
+                <TouchableOpacity onPress={this.pressAction.bind(this)}>
+                    <View style={{width:100,height:50,backgroundColor:'black'}}>
+                        <Text style={{color:'white',textAlign:'center',lineHeight:50}}>点击开始</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            
+           
+            
+
+        )
+
+    }
+
+}
+
 
 
 export default class LKAnimation extends Component {
@@ -97,6 +221,7 @@ export default class LKAnimation extends Component {
                 <ScringView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
                     <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
                 </ScringView>
+                <LKLayoutView style={{marginTop: 20,}}></LKLayoutView>
             </ScrollView>
         )
     }
